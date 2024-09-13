5b0b74c03f1656c69ef9b562b814299c0cc44061
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-09-12 21:30:31 -0300

    Começando a trabalhar com a API

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 const searchCity = document.getElementById('searchCity')
+const buttonSearch = document.getElementById('buttonSearch')
 
-function updateWeather() { 
-    fetch('http://api.weatherapi.com/v1/current.json?key=2f8b9850c91c4231ab8231351241209&q=London')
+
+
+buttonSearch.addEventListener('click',displayWeather)
+
+function updateWeather(cityName) { 
+    fetch(`http://api.weatherapi.com/v1/current.json?key=2f8b9850c91c4231ab8231351241209&q=${cityName}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
 }
+
+function displayWeather() {
+    let cityName = searchCity.value
+
+    updateWeather(cityName)
+}
+
+
 
 
 
