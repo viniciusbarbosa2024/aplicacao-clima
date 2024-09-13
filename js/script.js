ca39e06e0e29b4f80479718abcf01129a5c83f03
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-09-12 21:22:58 -0300

    iniciando js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const searchCity = document.getElementById('searchCity')
+
+function updateWeather() { 
+    fetch('http://api.weatherapi.com/v1/current.json?key=2f8b9850c91c4231ab8231351241209&q=London')
+}
+
+
+
+
