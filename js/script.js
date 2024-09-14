a07237a2e29a515f9257ea62a76d965396dee6d8
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-09-13 21:13:18 -0300

    Organizei o código

--- js/script.js
+++ js/script.js
@@ -16,15 +16,16 @@ async function updateWeather(cityName) {
     const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=2f8b9850c91c4231ab8231351241209&q=${cityName}`)
     const data = await res.json()
 
-    let temp = data.current.temp_c
-    temperature.innerHTML = `${temp.toLocaleString('pt-br')}°`
-    
+    return data
 }
 
-function displayWeather() {
+async function displayWeather() {
     let cityName = searchCity.value
 
-    updateWeather(cityName)
+    const data = await updateWeather(cityName)
+
+    let temp = data.current.temp_c
+    temperature.innerHTML = `${temp.toLocaleString('pt-br')}°`
 }
 
 
