28431fa58a467912f5095abba548bb27a8c01aa6
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-09-13 21:08:13 -0300

    Exibição do clima de uma cidade x, funcional

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 const searchCity = document.getElementById('searchCity')
 const buttonSearch = document.getElementById('buttonSearch')
-
+const temperature = document.getElementById('temperature')
 
 
 buttonSearch.addEventListener('click',displayWeather)
 
-function updateWeather(cityName) { 
-    fetch(`http://api.weatherapi.com/v1/current.json?key=2f8b9850c91c4231ab8231351241209&q=${cityName}`)
-    .then(res => res.json())
-    .then(data => console.log(data))
+fetch(`http://api.weatherapi.com/v1/current.json?key=2f8b9850c91c4231ab8231351241209&q=Caruaru`)
+.then((res) => res.json())
+.then((data) => {
+    let temp = data.current.temp_c
+    temperature.innerHTML = `${temp.toLocaleString('pt-br')}°`
+})
+
+async function updateWeather(cityName) { 
+    const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=2f8b9850c91c4231ab8231351241209&q=${cityName}`)
+    const data = await res.json()
+
+    let temp = data.current.temp_c
+    temperature.innerHTML = `${temp.toLocaleString('pt-br')}°`
+    
 }
 
 function displayWeather() {
